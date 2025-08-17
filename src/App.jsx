b508b9145d8e1a4b58da74eb579d0cab19a5286b
Shashi Bhsuhan <shashibhushan847305@gmail.com>
2025-08-17 23:38:48 +0530

    route setup understood

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
 import { Button } from './components/ui/button'
 import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import AppLayout from './Layout/AppLayout'
+import LandingPage from './Pages/LandingPage'
+import Onboarding from './Pages/Onboarding'
+import path from 'path'
 
 function App() {
   
-
+const Router=createBrowserRouter(
+  [
+    {element:<AppLayout />,
+      children:[
+       {
+        path:'/',
+        element:<LandingPage />
+       },
+       {
+        path:'/onboarding',
+        element:<Onboarding />
+       }
+      ]
+      
+    }
+  ]
+)
   return (
-    <>
-     
-      <Button>Login</Button>
-     
-    </>
+    <RouterProvider router={Router} />
+   
   )
 }
 
